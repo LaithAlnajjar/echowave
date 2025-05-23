9d2c0422d0d1ab063ee1d2ffdb48475c7feff236
Laith Alnajjar <laithalnajjar2004@gmail.com>
2025-05-23 14:20:48 +0300

    chore(server): add session and passport middleware with Prisma store

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,33 @@
 import express, { Request, Response } from "express";
 import { authRouter } from "./routes/authRoutes";
+import passport from "passport";
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
+
+const SESSION_SECRET = process.env.SECRET;
+if (!SESSION_SECRET) {
+  throw new Error("SESSION SECRET is not defined in environment variables.");
+}
+
+app.use(
+  session({
+    cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 },
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/auth", authRouter);
 
